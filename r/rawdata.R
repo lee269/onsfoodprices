b0f847a4571d7bb90aaf5da416ad564019158e36
David Lee <david@lee269.co.uk>
2020-10-17 16:20:15 +0100

    Better dealing with raw data quirks

--- r/rawdata.R
+++ r/rawdata.R
@@ -8,10 +8,14 @@ pacman::p_load(here, tidyverse, tidyxl, unpivotr, lubridate)
 
 extract_indices <- function(xlsx) {
 
+  # the filter rows where numeric is NA and character  is "" is required
+  # because ONS puts spaces in the 'blank' cells under each Category heading
+  
   tidy_data <- 
     tidyxl::xlsx_cells(xlsx) %>% 
     dplyr::filter(sheet == "Indices") %>% 
     dplyr::filter(!is_blank, row > 1 & row <= 155) %>% 
+    dplyr::filter(!is.na(numeric) | character != "") %>% 
     unpivotr::behead("up", week) %>% 
     unpivotr::behead("left-up", category) %>% 
     unpivotr::behead("left", item) %>%
@@ -22,7 +26,7 @@ extract_indices <- function(xlsx) {
 }
 
 
-rawxls <- here("data", "raw", "8thoctoberonlineweeklypricesdatasetcleaned.xlsx")
+rawxls <- here("data", "raw", "15octoberpricesdataset.xlsx")
 tidy_prices <- extract_indices(rawxls)
 
 saveRDS(tidy_prices, file = here("data", "tidy", "pricedata.rds" ))
